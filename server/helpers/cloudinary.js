d93fb44abc9e69c229be77e4a6c538a3ac8191e8
IRSHAD-72 <alonekiing28@gmail.com>
2025-05-20 17:54:27 +0530

    Update cloudinary.js

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dmtawwssh",
-  api_key: "779949263232281",
-  api_secret: "Zt9ETKQeTDXRkxFuo1rUuTojMvM",
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
 });
 
 const storage = new multer.memoryStorage();
