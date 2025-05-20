d565e6a094d4d611f4d159c04b3d04c278c4a86a
IRSHAD-72 <alonekiing28@gmail.com>
2025-05-20 17:57:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("mongodb+srv://irshad:irshadsheikh@cluster1.d60cj.mongodb.net/?retryWrites=true&w=majority&appName=cluster1")
+  .connect("")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
